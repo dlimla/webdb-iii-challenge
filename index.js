66012c20c6e25ac158ed3cda9c9e1f46d0dcae5f
dlimla <danielhlim1990@gmail.com>
2019-03-19 18:54:26 -0700

    adds server on the package.json server is up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile.js')
+
+const db = knex(dbConfig.development);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const port = 4000;
+
+server.listen(port, () => 
+    console.log(`\nrunning on ${port}\n`)
+);
